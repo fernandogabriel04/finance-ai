e56bcb9f2c3a505f8299af675976680546b90190
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-01-03 15:54:31 -0300

    feat: ensure transactions and subscriptions pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,12 @@
+import { auth } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
+import { redirect } from "next/navigation";
 
-const SubscriptionsPage = () => {
+const SubscriptionsPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return <Navbar />;
 };
 
