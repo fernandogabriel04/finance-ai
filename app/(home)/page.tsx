9ebdc675ae159ca01e03bd5cc8629669b98d3da5
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-01-19 17:52:30 -0300

    fix: adjust scrolling in home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -38,10 +38,10 @@ const Home = async ({ searchParams: { period } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <div className="grid grid-cols-[2fr,1fr] gap-6">
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6 overflow-hidden">
             <SummaryCards month={month} {...dashboard} />
-            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+            <div className="grid grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
               <TransactionsPieChart {...dashboard} />
               <ExpensesPerCategory
                 expensesPerCategory={dashboard.totalExpensePerCategory}
