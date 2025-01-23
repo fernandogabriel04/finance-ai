23c0d19a5b00f7051486e5ced3d6cae6d583013c
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2025-01-23 14:31:57 -0300

    feat: ensure user can not create transaction on free plan if above limit

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,6 +7,7 @@ import TransactionsPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "./_data/get-dashboard";
 import ExpensesPerCategory from "./_components/expenses-per-category";
 import LastTransactions from "./_components/last-transactions";
+import { canUserAddTransaction } from "./_data/can-user-add-transaction";
 
 interface HomeProps {
   searchParams: {
@@ -29,7 +30,7 @@ const Home = async ({ searchParams: { period } }: HomeProps) => {
     redirect(`/?period=${defaultPeriod}`);
   }
   const dashboard = await getDashboard(month, year);
-
+  const userCanAddTransaction = await canUserAddTransaction();
   return (
     <>
       <Navbar />
@@ -40,7 +41,11 @@ const Home = async ({ searchParams: { period } }: HomeProps) => {
         </div>
         <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
           <div className="flex flex-col gap-6 overflow-hidden">
-            <SummaryCards month={month} {...dashboard} />
+            <SummaryCards
+              month={month}
+              {...dashboard}
+              userCanAddTransaction={userCanAddTransaction}
+            />
             <div className="grid grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
               <TransactionsPieChart {...dashboard} />
               <ExpensesPerCategory
